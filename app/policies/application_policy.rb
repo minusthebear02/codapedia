6c00dabe8ae906d75b24e8802c920ae53f112bf8
Mark Froehlich <mfroeh2@gmail.com>
2017-07-14 08:28:42 -0400

    Removed delete button except for author and admin

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,9 +1,9 @@
 class ApplicationPolicy
-  attr_reader :user, :record
+  attr_reader :user, :wiki
 
-  def initialize(user, record)
+  def initialize(user, wiki)
     @user = user
-    @record = record
+    @wiki = wiki
   end
 
   def index?
@@ -11,7 +11,7 @@ class ApplicationPolicy
   end
 
   def show?
-    scope.where(:id => record.id).exists?
+    scope.where(:id => wiki.id).exists?
   end
 
   def create?
@@ -31,11 +31,11 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+   user.admin? || @wiki.user == current_user
   end
 
   def scope
-    Pundit.policy_scope!(user, record.class)
+    Pundit.policy_scope!(user, wiki.class)
   end
 
   class Scope
