af83c0837c9d5e7ed7cca37a703831beed80f4f9
Mark Froehlich <mfroeh2@gmail.com>
2017-08-29 23:18:39 -0400

    spec:implemented

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe WikisController, type: :controller do
+  describe "GET index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET show" do
+    it "renders the show template" do
+      get :show
+      expect(response).to render_template("show")
+    end
+  end
 
 end
